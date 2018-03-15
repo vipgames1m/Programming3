37f1b23c12ac8246c2b5feca0bcc288d7145ce8e
user <user@comp14m.tumo.lab>
2018-03-15 17:35:40 +0400

    Lesson 1

---
+++ Nujn banery 1 mej.js
@@ -0,0 +1,21 @@
+    constructor(x, y, ind) {
+        this.index = ind;
+        this.x = x;
+        this.y = y;
+      
+        this.multiply = 0;
+
+    }
+
+        newDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
